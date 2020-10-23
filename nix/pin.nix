d7d9b381234d0ce48a025e9fbbe04cf92dd4f602
Jappie Klooster <jappieklooster@hotmail.com>
2020-10-23 12:28:41 +0200

    Bump nixpkgs see if that works

--- nix/pin.nix
+++ nix/pin.nix
@@ -1,6 +1,6 @@
 import (builtins.fetchGit {
     # Descriptive name to make the store path easier to identify
-    name = "nixos-pin-19.08.2020";
+    name = "nixos-pin-23.10.2020";
     url = https://github.com/nixos/nixpkgs/;
-    rev = "6baf442a9e1336d46668fbb4564fa9dc9b5ae1c5";
+    rev = "b36db49ae73db879fe00f73f2777077f33a68745";
     })
